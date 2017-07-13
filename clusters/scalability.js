6fce5fc5f60c9c787232c0f6027a801229dfe194
Victor <victoraweb@gmail.com>
2017-07-13 10:41:12 -0300

    [DOCS] Sticky load balancing

--- clusters/scalability.js
+++ clusters/scalability.js
@@ -8,3 +8,10 @@
 // 2. fork() - 
 // 3. exec() - Creates a shell. Buffers the generated command output and pass the whole value to a callback;
 // 4. execFile() - To execute a file.
+
+// Drawbacks of load balance:
+// We cannot cache data in memory because each worker has its own memory space. To store something in cache we must use other application. E.g., redis.
+// Stateful comunication becomes a problem. Each worker has its own state. So if a user logged in a worker, other would not know about it. The best to do here is also use redis.
+//  In case redis cannot be used, you can use the approach Sticky Load Balancing. When a user authenticates with a worker instance, we keep a record of that relation on the Load Balancer level.
+//  So the next time the user makes a request again, we look up the records and send the user to the same worker.
+//  The cluster module does not support sticky load balancing.
